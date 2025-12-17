e77bc33a3af7ad574f90ebcad1d1a35bf94baaa6
Vivek Raj <vivekchaudhary1708gmail.com>
2025-12-17 22:58:30 +0530

    Added basic programming elements Java programs

---
+++ core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AddTwoNumbers{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the first number: ");
+        int b = sc.nextInt();
+        int result = a+ b;
+        System.out.print(result);
+
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/CircleArea.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CircleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        double area = Math.PI * r * r;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
