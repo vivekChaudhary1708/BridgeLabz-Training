81ff096243e59de7fbc3f35b3162c2e96945d825
Vivek Raj <vivekchaudhary1708gmail.com>
2025-12-30 22:33:27 +0530

    Remove unwanted folders from oops-practice

--- gcr-codebase/Level-1-codes/Dob.java
+++
@@ -1,8 +0,0 @@
-class Dob{
-    public static void main(String[] args){
-        int dob =2000;
-        int currentyear=2024;
-        int age=currentyear-dob;
-        System.out.println("Your age is: " + age);
-    }
-}
--- gcr-codebase/strings/FunctionsWithoutReturn.java
+++
@@ -1,3 +0,0 @@
-static int add(int a, int b) {
-    return a + b;
-}
